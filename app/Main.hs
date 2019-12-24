3cb39518020ef686fbda5414e01c5af068444974
Hansen <hansenchan1995@gmail.com>
2019-12-24 17:44:28 +0800

    Implemented play error handling and win check and command system, next step: save

--- app/Main.hs
+++ app/Main.hs
@@ -21,24 +21,27 @@ data Board = Board (Array (Int, Int) Int) (Array (Int, Int) Int) deriving (Eq)
 
 -- Display Board
 -- TODO: output as in pdf file
+displayBoard :: Board -> [Char]
+displayBoard board = show board
+
 printArray :: ( Array (Int,Int) Int ) -> String
 printArray arr = unlines [unwords [show (arr ! (x, y)) | x <- [0..8]] | y <- [0..8]]
 
 instance Show Board where
     show (Board num loc) = "number: \n" ++ printArray num ++ "\n location: \n" ++ printArray loc
-  
--- other checking should be defined
--- https://www.tutorialspoint.com/haskell/haskell_types_and_type_class.htm
--- last one
 
 -- Load Board
-rawBoard = readFile "../../final project/map.txt" >>= return  . map scanString . lines
+rawBoard = readFile "../../final project/map2.txt" >>= return  . map scanString . lines
 
 load = rawBoard >>= \b -> return (Board (array ((0,0),(8,8)) (arrayConstructor (map ((!!) b) [9..17]))) (array ((0,0),(8,8)) (arrayConstructor (map ((!!) b) [0..8]))))
 
 -- TODO: keep asking for make move
 -- Like in Hangman.hs
 testMove = load >>= \b -> return (move b 1 1 2)
+
+testMoveError1 = load >>= \b -> return (move b 0 1 2)
+
+testMoveError2 = load >>= \b -> return (move b 0 0 8)
 
 scanChar :: Char -> Int
 scanChar c | '0' <= c && c <= '9' = fromEnum c - fromEnum '0'
@@ -54,31 +57,55 @@ arrayConstructor :: [[Int]] -> [((Int, Int), Int)]
 arrayConstructor [[]] = [((0,0),0)]
 arrayConstructor m = [ ((x,y),m!!y!!x) | x <- [0..8], y <- [0..8] ]                   
 
--- Make Move with basic checking including the range of location(0-8), number to be inserted (1-9) and the location is empty or not
+-- Make Move with Jigsaw Sudoku game rules checking 
 move :: Board -> Int -> Int -> Int -> Board
-move (Board num loc) x y n | 0 <= x && x <= 8 
-                                && 0 <= y && y <= 8 
-                                && 1 <= n && n <= 9
-                                && (num ! (x,y)) == -1
-                                = Board (num // [((x,y), n)]) loc
+move (Board num loc) x y n | check (Board num loc) x y n = Board (num // [((x,y), n)]) loc
                             | otherwise = (Board num loc)
 
--- TODO: further error handling for make move
--- The same line/row does not contain the number you are trying to insert.
+-- Implement checking including the range of location(0-8), number to be inserted (1-9), the location is empty or not,
+-- and the same line/row contains the number you are trying to insert or not.                            
+check :: Board -> Int -> Int -> Int -> Bool
+check (Board num loc) x y n = 0 <= x && x <= 8 && 0 <= y && y <= 8 && 1 <= n && n <= 9 && (num ! (x,y)) == -1
+                                   && not (elem n (map (\((_,_),z) -> z) (filter (\((a,_),_) -> a == x) (assocs num))))
+                                   && not (elem n (map (\((_,_),z) -> z) (filter (\((_,a),_) -> a == y) (assocs num))))
+                                   && jigsawBlockCheck (Board num loc) x y n
 
+-- The same Jigsaw Sudoku block does not contain the number you are trying to insert.
+jigsawBlockCheck :: Board -> Int -> Int -> Int -> Bool                                  
+jigsawBlockCheck (Board num loc) x y n = not (elem n (map ((!) num) (map (\((a,b),_) -> (a,b)) (filter (\((_,_),c) -> c == loc ! (x,y)) (assocs loc)))))
 
--- TODO: further error handling for make move
--- The same box does not contain the number you are trying to insert.
-
--- Auxiliary function
--- get the location of number in the same box
-
-
+-- Decide whether the player win or not
+jigsawSudokuCheck :: Board -> Bool
+jigsawSudokuCheck (Board num loc) = not (elem (-1) (elems num))
 
 -- Save Board
 -- TODO: write file with Board Object                    
 
--- main access point                    
-main = putStrLn "Jigsaw Sudoku">>=
-        return load
+-- main access point
+main :: IO ()                    
+main = putStrLn "Jigsaw Sudoku">>= \_ ->
+            load >>= \b -> play b
+
+
+
+-- demo play game
+
+play :: Board -> IO ()
+play sudoku = putStr (displayBoard sudoku) >>= \_ ->
+                putStrLn "Please enter command: " >>= \_ ->
+                    getLine >>= \cmd ->
+                        case cmd of 
+                            "move" -> putStrLn "Row: " >>= \_ -> getChar >>= \y -> putStrLn "Column: " >>= \_ -> getChar >>= \x -> putStrLn "Number: " >>= \_ -> getChar >>= \n ->
+                                                    return (move sudoku (scanChar x) (scanChar y) (scanChar n)) >>= \b ->
+                                                        if (jigsawSudokuCheck b) then
+                                                            putStrLn "You win!"
+                                                         else 
+                                                            play b
+                            "quit" -> putStrLn "Bye"
+                            "save" -> putStrLn "To be implemented"
+                            _ -> play sudoku                                
+
+
+
+                                            
 
