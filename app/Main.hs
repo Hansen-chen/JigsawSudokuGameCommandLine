f36f25f4a2390a5ee6843a99b7e4590a0dec7605
Hansen <hansenchan1995@gmail.com>
2019-12-24 13:28:58 +0800

    finish load, make move. But need to do in IO type or in monad

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,9 @@
 module Main where
 
--- deal with stack tool    
--- import Data.Array
+-- deal with stack tool when implementing GUI  
+import Data.Array
+import System.IO
+
 
 -- construction
 -- table = array ((1,1),(3,3)) [ ((1,1), [1,2]), ((1,2), [1,2]), ((1,3), [1,2]), ((2,1), [1,2]), ((2,2), [1,2]), ((2,3), [1,2]), ((3,1), [1,2]), ((3,2), [1,2]), ((3,3), [1,2]) ]
@@ -13,24 +15,28 @@ module Main where
 -- table ! (1,1)
 
 
--- List of Int records number in the block, -1 as not filled
--- The second list records the board
-data Block = Block [[Int]] [[Int]] deriving (Eq)
+-- First matrix records number in the Board, -1 as not filled
+-- The second matrix records the board location
+data Board = Board (Array (Int, Int) Int) (Array (Int, Int) Int) deriving (Eq)
 
 -- Display Board
 -- TODO: output as in pdf file
-instance Show Block where
-    show (Block loc num) = "location: " ++ show loc ++ "\n number:" ++ show num
+printArray :: ( Array (Int,Int) Int ) -> String
+printArray arr = unlines [unwords [show (arr ! (x, y)) | x <- [0..8]] | y <- [0..8]]
+
+instance Show Board where
+    show (Board num loc) = "number: \n" ++ printArray num ++ "\n location: \n" ++ printArray loc
   
 -- other checking should be defined
 -- https://www.tutorialspoint.com/haskell/haskell_types_and_type_class.htm
 -- last one
 
 -- Load Board
--- 2d array element access: (a!!1)!!2
 rawBoard = readFile "../../final project/map.txt" >>= return  . map scanString . lines
 
-load = rawBoard >>= \b -> return (Block (map ((!!) b) [0..8]) (map ((!!) b) [9..17]))
+load = rawBoard >>= \b -> return (Board (array ((0,0),(8,8)) (arrayConstructor (map ((!!) b) [9..17]))) (array ((0,0),(8,8)) (arrayConstructor (map ((!!) b) [0..8]))))
+
+testMove = load >>= \b -> return (move b 1 1 2)
 
 scanChar :: Char -> Int
 scanChar c | '0' <= c && c <= '9' = fromEnum c - fromEnum '0'
@@ -42,22 +48,24 @@ scanString (x:xs) | 0 <= sc && sc <= 9 = sc:(scanString xs)
                   | otherwise = -1:(scanString xs)
                     where sc = scanChar x
 
--- Save Board
--- TODO: write file with Block Object                    
+arrayConstructor :: [[Int]] -> [((Int, Int), Int)]
+arrayConstructor [[]] = [((0,0),0)]
+arrayConstructor m = [ ((x,y),m!!y!!x) | x <- [0..8], y <- [0..8] ]                   
 
 -- Make Move
--- TODO: further error handling?
--- or use matrix
-move :: Block -> Int -> Int -> Int -> Block
-move (Block loc num) x y n | 0 <= x && x <= 9 
-                                && 0 <= y && y <= 9 
-                                && 0 <= n && n <= 9 
---                                = Block loc (num & element y . element x .~ n)
-                                = (Block loc num)
-                            | otherwise = (Block loc num)
-                            
+move :: Board -> Int -> Int -> Int -> Board
+move (Board num loc) x y n | 0 <= x && x <= 8 
+                                && 0 <= y && y <= 8 
+                                && 1 <= n && n <= 9 
+                                = Board (num // [((x,y), n)]) loc
+                            | otherwise = (Board num loc)
 
+-- TODO: further error handling for make move
+
+-- Save Board
+-- TODO: write file with Board Object                    
 
 -- main access point                    
 main = putStrLn "Jigsaw Sudoku">>=
         return load
+
